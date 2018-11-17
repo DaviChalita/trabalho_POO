9e3d79f463215e6076e7b1d0003bb5d93c9961bd
Samir Saadi <samirsaadi@gmail.com>
2018-11-17 19:46:18 -0200

    Financeiro

---
+++ Financeiro.java
@@ -0,0 +1,13 @@
+package bolo;
+
+public class Financeiro {
+    private booelan capital;
+
+    public booelan getCapital() {
+        return capital;
+    }
+
+    public void setCapital(booelan capital) {
+        this.capital = capital;
+    }
+}
